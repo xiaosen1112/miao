e3c957df993d83be846dec7d4e25aabe264ae924
xiaosen <2389699530@qq.com>
2022-09-15 10:34:58 +0800

    first commit

--- lodash/xiaosen1112-lodash.js
+++ lodash/xiaosen1112-lodash.js
@@ -11,19 +11,29 @@ var xiaosen1112 = {
         index = 0
       }
       index++
+      map.push(array[i])
     }
-    map.push(array[i])
     if (index !== 0) {
       result.push(map)
       map = []
       index = 0
     }
     return result
-  }
+  },
 
+  compact: function compact(array) {
+    var result = []
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] && array[i] !== 0) {
+        result.push(array[i])
+      }
+    }
+    return result
+  },
 
-
-
+  difference: function difference(array, values) {
+    
+  },
 
 
 
